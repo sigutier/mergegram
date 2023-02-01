acfe1a3fe9a378ef4109ef8ff22d0fea41fd8bc9
Pilar <pilar.casanueva@gmail.com>
2023-02-01 20:37:01 +0100

    Add first views

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,11 @@
+
+
+<template>
+    <h1>Iniciar sesión (componente)</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({})
+</script>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Iniciar sesión</h1>
+    <LoginComponent />
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue";
+import LoginComponent from "@/components/LoginComponent.vue";
+
+export default defineComponent({
+    components: {
+    LoginComponent
+},
+})
+
+</script>
