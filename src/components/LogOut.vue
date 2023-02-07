86483c45e049b786eb6c3c98e68d38140a59a083
sigutier <slvgutierrezrodriguez@gmail.com>
2023-02-07 18:22:08 +0100

    add LogOut component

---
+++ src/components/LogOut.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="cerrar">
+        <button @click="logout">Cerrar sesión</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue';
+import { useRouter } from 'vue-router';
+import IStorage from '@/interfaces/Storage';
+
+export default defineComponent({
+    setup() {
+        const router = useRouter();
+        const Storage = inject<IStorage>('Storage');
+        const logout = (): void => {
+            Storage?.delete();
+            router.push('/login');
+        };
+        return {
+            logout,
+        }
+    }
+});
+</script>
