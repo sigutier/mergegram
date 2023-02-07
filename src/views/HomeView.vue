78650070b4f1c00bd3cd2e39f4694f86fa881c83
sigutier <slvgutierrezrodriguez@gmail.com>
2023-02-07 18:21:31 +0100

    add components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,36 +2,40 @@
   <div class="home">
     <section class="contactList">
       Contactos
-      <BrowseInput />
+      <InputSearch />
       <ContactList />
     </section>
     <section class="messages">
-      <MessagesList />
+      <LogOut />
+      <MessageList />
+      <InputMessage />
     </section>
-    <InputMessage/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import BrowseInput from "@/components/BrowseInput.vue";
+import InputSearch from "@/components/InputSearch.vue";
 import ContactList from "@/components/ContactList.vue";
-import MessagesList from "@/components/Messages/MessageList.vue";
+import MessageList from "@/components/Messages/MessageList.vue";
 import InputMessage from '@/components/InputMessage.vue';
+import LogOut from '@/components/LogOut.vue';
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    BrowseInput,
+    InputSearch,
     ContactList,
-    MessagesList,
+    MessageList,
     InputMessage,
+    LogOut
   },
 });
 </script>
 
 <style lang="css" scoped>
 @import url("../assets/variables.css");
+
 .home {
   background: var(--backgroundColor);
   width: 100%;
@@ -42,11 +46,13 @@ export default defineComponent({
   align-items: flex-start;
   justify-content: center;
 }
+
 .contactList {
   width: 35%;
   display: flex;
   flex-direction: column;
 }
+
 .messages {
   width: 65%;
   background: var(--backgroundColorMessages);
