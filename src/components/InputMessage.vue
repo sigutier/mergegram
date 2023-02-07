a444fbc1ac1b7fbd27be35ce728e72dc51d13130
sigutier <slvgutierrezrodriguez@gmail.com>
2023-02-07 18:16:35 +0100

    add InputMessage component

--- src/components/InputMessage.vue
+++ src/components/InputMessage.vue
@@ -1,33 +1,55 @@
 <template>
     <div class="sendMessage">
-       <input type="text" v-model="text" @keypress.enter="sendMessage" width="100%" placeholder="Escribe tu mensaje...">
+        <!-- <input type="text" v-model="text" @keypress.enter="sendMessage" width="100%"
+            placeholder="Escribe tu mensaje..."> -->
+        <discord-picker input :value="text" @update:value="text = $event" @emoji="setEmoji"
+            @keypress.enter="sendMessage" />
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref} from "vue";
-import { db, firebase } from '@/firebase';
+import { defineComponent, ref } from "vue";
+import DiscordPicker from 'vue3-discordpicker';
 import { useStore } from 'vuex';
+import { db, firebase } from '@/firebase';
 
 export default defineComponent({
     name: "InputMessage",
-    setup(){
+    components: {
+        DiscordPicker,
+    },
+    setup() {
         const text = ref<string>('');
+        const store = useStore();
         const cMessage = db.collection('messages');
+
+        const setEmoji = (emoji: any): void => {
+            console.log(emoji);
+        }
+        /* cMessage.onSnapshot((snapShot) => {
+            snapShot.docChanges().forEach((change) => {
+                if (change.type === 'added') {
+                    store.dispatch('getMessages');
+                }
+            });
+        }); */
+
         const sendMessage = async (): Promise<void> => {
+            const tmp = text.value;
+            text.value = '';
             cMessage.add({
-                type:'text',
-                text: text.value,
+                type: 'text',
+                text: tmp,
                 date: firebase.firestore.FieldValue.serverTimestamp(),
-                userUid: '',
+                userUid: store.state.userUid,
             });
         };
 
         return {
             text,
+            setEmoji,
             sendMessage,
         };
-    },
+    }
 });
-
 </script>
