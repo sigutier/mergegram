d9fced93c39cb25f9101b3fd130fa4d542616957
sigutier <slvgutierrezrodriguez@gmail.com>
2023-02-07 18:21:05 +0100

    add logged

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,49 +1,52 @@
-import { createStore } from 'vuex';
-import { db } from '@/firebase';
-import IMessage from '@/interfaces/Message';
+import { createStore } from "vuex";
+import { db } from "@/firebase";
+import IMessage from "@/interfaces/Message";
 
 export default createStore({
   state: {
-    userUid: '' as string,
+    userUid: "" as string,
     messages: [] as IMessage[],
+    logged: "" as string,
   },
 
-  getters: {
-  },
+  getters: {},
 
   mutations: {
     setUserUid(state: any, payload: any) {
       state.userUid = payload;
     },
+    setLogged(state: any, payload: any) {
+      state.logged = payload;
+    },
     setMessages(state: any, payload: any) {
-      state.messages= payload;
-    }
+      state.messages = payload;
+    },
   },
 
   actions: {
-    async getMessages ({commit, state}) {
+    async getMessages({ commit, state }) {
       try {
         const aMessages: IMessage[] = [];
-        const cMessages = await db.collection('messages').orderBy('date').where('userUid','==', state.userUid).get();
+        const cMessages = await db
+          .collection("messages")
+          .orderBy("date")
+          .where("userUid", "in", [state.userUid, state.logged])
+          .get();
         cMessages.forEach((data) => {
-          const { 
-            date, type, text, userUid
-          } = data.data();
+          const { date, type, text, userUid } = data.data();
           aMessages.push({
             date,
             type,
             text,
             userUid,
           });
-          commit('setMessages', aMessages);
-          console.log(aMessages);
         });
+        commit("setMessages", aMessages);
       } catch (error) {
         console.log(error);
       }
-    }
+    },
   },
 
-  modules: {
-  }
-})
+  modules: {},
+});
