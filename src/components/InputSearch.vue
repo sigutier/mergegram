7482b8fc991332001dccf7b5d56ccb504088f19c
Pilar <pilar.casanueva@gmail.com>
2023-02-15 12:07:45 +0100

    Add Message Search Functionality

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -1,13 +1,73 @@
 <template>
   <div class="search">
-    <input type="text" placeholder="Buscar..." class="input-search" />
+    <input type="text" placeholder="Buscar..." v-model="searchText" class="input-search"
+      @keyup.enter="searchMessages" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import { useStore } from "vuex";
+import { db } from "@/firebase";
+import IMessage from "@/interfaces/Message";
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    const store = useStore();
+    const searchText = ref<string>("");
+
+    const searchMessages = async (): Promise<void> => {
+
+      console.log(searchText.value);
+
+      const messages: IMessage[] = [];
+      const sentMessages = await db
+        .collection("messages")
+        .where("senderId", "==", store.state.user.uid)
+        .where("receiverId", "==", store.state.recipientUid)
+        .get();
+      sentMessages.forEach((data) => {
+        const { date, type, text, senderId, receiverId } =
+          data.data() as IMessage;
+        messages.push({
+          date,
+          type,
+          text,
+          senderId,
+          receiverId,
+          isReceiver: false,
+        });
+      });
+
+      const receivedMessages = await db
+        .collection("messages")
+        .where("senderId", "==", store.state.recipientUid)
+        .where("receiverId", "==", store.state.user.uid)
+        .get();
+      receivedMessages.forEach((data) => {
+        const { date, type, text, senderId, receiverId } =
+          data.data() as IMessage;
+        messages.push({
+          date,
+          type,
+          text,
+          senderId,
+          receiverId,
+          isReceiver: true,
+        });
+      });
+
+      console.log(messages.filter(x => x.text.includes(searchText.value)));
+      store.commit("setMessages", messages.filter(x => x.text.includes(searchText.value)));
+    };
+    return {
+      searchMessages,
+      searchText
+    };
+  }
+
+
+});
 </script>
 
 <style lang="css" scoped>
@@ -21,6 +81,7 @@ export default defineComponent({});
   outline: none;
   color: var(--fontColor);
 }
+
 .search input[type="text"]::placeholder {
   color: var(--fontColor);
 }
